eed7e5e306a4dc07525282e1e64a2b8d2ab353b7
zhangangeange <51976689+zhangangeange@users.noreply.github.com>
2019-07-10 14:49:29 +0800

    工具类

---
+++ Utils.java
@@ -0,0 +1,104 @@
+package com.taikang.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utils {
+
+	@Autowired
+	private StringRedisTemplate redisTemplate;
+
+	/**
+	 * 生成随机数
+	 * @return
+	 */
+	public String GenerateChdrnum() {
+		String date = TimeUtil.getCurrentDate("yyMMdd");
+		Long id = Long.parseLong(date) * 100000 + redisTemplate.opsForValue().increment("chdrnum");
+		return id.toString();
+	}
+	
+	 public String load(String url,String query) throws Exception
+    {
+        URL restURL = new URL(url);
+        /*
+         * 此处的urlConnection对象实际上是根据URL的请求协议(此处是http)生成的URLConnection类 的子类HttpURLConnection
+         */
+        HttpURLConnection conn = (HttpURLConnection) restURL.openConnection();
+        //请求方式
+        conn.setRequestMethod("POST");
+        //设置是否从httpUrlConnection读入，默认情况下是true; httpUrlConnection.setDoInput(true);
+        conn.setDoOutput(true);
+        //allowUserInteraction 如果为 true，则在允许用户交互（例如弹出一个验证对话框）的上下文中对此 URL 进行检查。
+        conn.setAllowUserInteraction(false);
+
+        PrintStream ps = new PrintStream(conn.getOutputStream());
+        ps.print(query);
+
+        ps.close();
+
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        String line,resultStr="";
+
+        while(null != (line=bReader.readLine()))
+        {
+        resultStr +=line;
+        }
+        System.out.println("3412412---"+resultStr);
+        bReader.close();
+
+        return resultStr;
+
+    }
+	
+	/**
+     * 调用对方接口方法
+     * @param path 对方或第三方提供的路径
+     * @param data 向对方或第三方发送的数据，大多数情况下给对方发送JSON数据让对方解析
+     */
+    public static void interfaceUtil(String path,String data) {
+        try {
+            URL url = new URL(path);
+            //打开和url之间的连接
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            PrintWriter out = null;
+            //请求方式
+//          conn.setRequestMethod("POST");
+//           //设置通用的请求属性
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)"); 
+            //设置是否向httpUrlConnection输出，设置是否从httpUrlConnection读入，此外发送post请求必须设置这两个
+            //最常用的Http请求无非是get和post，get请求可以获取静态页面，也可以把参数放在URL字串后面，传递给servlet，
+            //post与get的 不同之处在于post的参数不是放在URL字串里面，而是放在http请求的正文内。
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            //获取URLConnection对象对应的输出流
+            out = new PrintWriter(conn.getOutputStream());
+            //发送请求参数即数据
+            out.print(data);
+            //缓冲数据
+            out.flush();
+            //获取URLConnection对象对应的输入流
+            InputStream is = conn.getInputStream();
+            //构造一个字符流缓存
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String str = "";
+            while ((str = br.readLine()) != null) {
+                System.out.println(str);
+            }
+            //关闭流
+            is.close();
+            //断开连接，最好写上，disconnect是在底层tcp socket链接空闲时才切断。如果正在被其他线程使用就不切断。
+            //固定多线程的话，如果不disconnect，链接会增多，直到收发不出信息。写上disconnect后正常一些。
+            conn.disconnect();
+            System.out.println("完整结束");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
